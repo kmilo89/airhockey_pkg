f6ab5e94bffcb2fccf6f66f0ac00cf5ee23a731e
Camilo Londono <camilo89@hotmail.es>
2020-08-04 19:04:11 -0500

    node for get position disk and position puck

--- src/colors_detect_node.py
+++ src/colors_detect_node.py
@@ -10,7 +10,7 @@ from sensor_msgs.msg import CompressedImage, Image
 from airhockey_pkg.msg import camera
 from cv_bridge import CvBridge
 
-def draw(mask, color, image):
+def draw(mask, color, image, _area):
     found_object = False
     x = 0
     y = 0
@@ -18,7 +18,7 @@ def draw(mask, color, image):
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for c in contours:
         area = cv2.contourArea(c)
-        if area > 3000:
+        if area > _area:
             found_object = True
             m = cv2.moments(c)
             if m["m00"] == 0:
@@ -29,7 +29,7 @@ def draw(mask, color, image):
             font = cv2.FONT_HERSHEY_SIMPLEX
             new_contourn = cv2.convexHull(c)
             cv2.drawContours(image, [new_contourn], 0, color, 3)
-            cv2.putText(image, 'x:{}, y:{}'.format(x, y), (x+10, y), font,  0.75, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, 'x:{}, y:{}'.format(x, y), (x+10, y), font,  0.4, (0, 0, 0), 1, cv2.LINE_AA)
             area_object = area
     return image, x, y, found_object, area_object
 
@@ -41,8 +41,8 @@ def colors_detect(img):
     data.found_disk = False
     data.found_puck = False
     redLow1 = np.array([0, 100, 20], np.uint8)
-    redHigh1 = np.array([8, 255, 255], np.uint8)
-    redLow2 = np.array([175, 100, 20], np.uint8)
+    redHigh1 = np.array([10, 255, 255], np.uint8)
+    redLow2 = np.array([170, 100, 20], np.uint8)
     redHigh2 = np.array([179, 255, 255], np.uint8)
     blueLow = np.array([100, 100, 20], np.uint8)
     blueHigh = np.array([125, 255, 255], np.uint8)
@@ -51,12 +51,10 @@ def colors_detect(img):
     maskRed2 = cv2.inRange(frameHSV, redLow2, redHigh2)
     maskRed = cv2.add(maskRed1, maskRed2)
     maskBlue = cv2.inRange(frameHSV, blueLow, blueHigh)
-    img, data.pos_puck.x, data.pos_puck.y, data.found_puck, data.area_puck = draw(maskBlue, blue, img)
-    img, data.pos_disk.x, data.pos_disk.y, data.found_disk, data.area_disk = draw(maskRed, red, img)
+    img, data.pos_puck.x, data.pos_puck.y, data.found_puck, data.area_puck = draw(maskBlue, blue, img, 4000)
+    img, data.pos_disk.x, data.pos_disk.y, data.found_disk, data.area_disk = draw(maskRed, red, img, 4000)
     #new_img.data = cv2.imencode('.jpg', img)[1].tostring()
-    #bridge = CvBridge()
-    #new_img.data = bridge.imgmsg_to_cv2(img, cv2.CV_8UC1)[:, :, ::-1]
-    #new_img.data = img
+    new_img.data = bridge. cv2_to_imgmsg(img, "bgr8")
     pub_img.publish(new_img)
     pub.publish(data)
     cv2.imshow('frame', img)
